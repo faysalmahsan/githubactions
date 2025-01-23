ddfbd635c73617838a1bba1ed3da5ffc03779e5b
faysalahsan <faysal.ahsan@therapservices.net>
2025-01-24 00:55:52 +0600

    added linkting and formatting

--- app.py
+++ app.py
@@ -1,11 +1,15 @@
-# def sum():
-#     a = 5
-#     b = 10
-#     return a + b
+def sum():
+    a = 5
+    b = 10
+    return a + b
 
 
-# sum()
-def add(a, b): return a + b
+sum()
 
 
-result = add(5 + 10); print(result)
+def add(a, b):
+    return a + b
+
+
+result = add(5 + 10)
+print(result)
