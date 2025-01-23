dd4ed58ad44a7e554d41bc812ecd0272545f55ea
faysalahsan <faysal.ahsan@therapservices.net>
2025-01-24 00:50:21 +0600

    Updated app.py file

--- app.py
+++ app.py
@@ -1,5 +1,11 @@
-def sum():
-    a = 5
-    b = 10
-    return a +c
-sum()
+# def sum():
+#     a = 5
+#     b = 10
+#     return a + b
+
+
+# sum()
+def add(a, b): return a + b
+
+
+result = add(5 + 10); print(result)
